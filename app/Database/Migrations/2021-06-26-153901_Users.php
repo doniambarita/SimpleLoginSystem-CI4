8a588826ae3df5cec830ca09b3f06fa202e3accb
doniambarita <doniambarita123@gmail.com>
2021-06-26 23:04:45 +0700

    menambahkan migration

---
+++ app/Database/Migrations/2021-06-26-153901_Users.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Webinar extends Migration
+{
+	public function up()
+	{
+			$this->forge->addField([
+				'id' => [
+					'type'           => 'INT',
+					'constraint'     => 11,
+					'unsigned'       => true,
+					'auto_increment' => true,
+				],
+				'firstname'    => [
+					'type'           => 'VARCHAR',
+					'constraint'     => '50',
+				],
+				'lastname'     => [
+					'type'           => 'VARCHAR',
+					'constraint'     => '60',
+				 ],
+				 'email'       => [
+					'type'           => 'VARCHAR',
+					'constraint'     => '100',
+				],
+				   'password'  => [
+					'type'           => 'VARCHAR',
+					'constraint'     => '255',
+				],
+				'created_at'   => [
+					'type'           => 'DATETIME',
+					'default'        => 'current_timestamp()',
+				],
+				'updated_at'   => [
+					'type'           => 'DATETIME',
+					'default'        => 'current_timestamp()',
+				]
+				]);
+		$this->forge->addKey('id', TRUE);
+		$this->forge->createTable('users');
+	}
+
+	public function down()
+	{
+			$this->forge->dropTable('users');
+	}
+}
